9b7a4c17db5ae59105b418bb8224417cd41a9b04
AnisaAlifiaFauziah <anisaalifia41@gmail.com>
2022-09-29 11:01:33 +0700

    Contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</>";
+        echo "Nama saya Imam Nawawi
+            Saya tinggal di daerah Ciputat
+            olahraga yang saya sukai adalah
+            Bulutangkis";
+    }
+}
